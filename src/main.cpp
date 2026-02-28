cf94c179e8b75ad3db87d0a555c7272862de3ea4
Shreya-0007 <ss0684397@gmail.com>
2026-02-28 23:19:08 +0530

    System initialization messages printed to Serial Monitor

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,7 @@
  */
 
 // TODO 1: Define touch sensor digital pin (Use pin 2)
-const int TOUCH_PIN = 2;
+const int TOUCH_PIN = 7;
 
 // TODO 2: Create variable to store touch state
 int touchState = 0;
